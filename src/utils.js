6a901bdb94f5323c7ef363aedcc5ca087cb76ed5
teabyii <teabyii@gmail.com>
2017-03-30 00:57:57 +0800

    Add attach function in utils

--- src/utils.js
+++ src/utils.js
@@ -12,7 +12,7 @@ export const win = window || {
   }
 }
 
-const Node = win.Node
+export const Node = win.Node
 
 /**
  * A element node or document node, or document fragment node?
@@ -31,4 +31,42 @@ export function isGoodNode (node) {
   return type === Node.ELEMENT_NODE ||
     type === Node.DOCUMENT_NODE ||
     type === Node.DOCUMENT_FRAGMENT_NODE
+}
+
+// https://infra.spec.whatwg.org/#ascii-whitespace
+export const RE_NOTHTMLWHITE = /[^\x20\t\r\n\f]+/g
+
+/**
+ * Strip and collapse whitespace according to HTML spec
+ * https://infra.spec.whatwg.org/#strip-and-collapse-ascii-whitespace
+ *
+ * @export
+ * @param {string} value
+ * @returns {string}
+ */
+export function stripAndCollapse (value) {
+  return (value.match(RE_NOTHTMLWHITE) || []).join(' ')
+}
+
+// Unique identification for QLite in each page.
+export const expando = `QLite${Math.random()}`.replace(/\D/g, '')
+
+export function attach (owner, key, value) {
+  let store = owner[expando]
+
+  // Check if the owner object already has a store
+  if (!store) {
+    store = owner[expando] = {}
+  }
+
+  if (!key) {
+    return store
+  }
+
+  if (!value) {
+    return store[key]
+  } else {
+    store[key] = value
+    return value
+  }
 }
--- test/inside/utils.js
+++ test/inside/utils.js
@@ -1,8 +1,22 @@
 import assert from 'assert'
-import { isGoodNode } from '../../src/utils'
+import {
+  win,
+  isGoodNode,
+  attach
+} from '../../src/utils'
 
 describe('Utils', () => {
   it('node', () => {
     assert(!isGoodNode())
+  })
+
+  it('attach', () => {
+    const doc = win.document
+    const data = attach(doc)
+    assert.equal(typeof data, 'object')
+    assert.equal(attach(doc), data)
+
+    attach(doc, 'a', 1)
+    assert.equal(attach(doc, 'a'), 1)
   })
 })
